a1fb4eeda07cf77b24540cac8eab55e64e5e04c4
subhashmahamkali <mahankalisubhash03@gmail.com>
2024-02-27 13:25:49 -0600

    PCA

---
+++ profiling/2_PCA.R
@@ -0,0 +1,17 @@
+#https://speciationgenomics.github.io/pca/
+#pca was conducted using VCF file susing plink.
+
+library(tidyverse)
+# read in data
+pca <- read_table2("./sorghum.eigenvec", col_names = FALSE)
+eigenval <- scan("./sorghum.eigenval")
+
+# sort out the pca data
+# remove nuisance column
+pca <- pca[,-1]
+# set names
+names(pca)[1] <- "id"
+names(pca)[2:ncol(pca)] <- paste0("PC", 1:(ncol(pca)-1))
+library(ggplot2)
+ggplot(data=pca, aes(PC1,PC2)) + geom_point()
+
