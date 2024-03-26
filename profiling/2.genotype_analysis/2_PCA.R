a9719d0a6e61173a9888020b7fdca21e0e3d03ed
subhashmahamkali <mahankalisubhash03@gmail.com>
2024-03-26 00:29:15 -0500

    pca

--- profiling/2.genotype_analysis/2_PCA.R
+++ profiling/2.genotype_analysis/2_PCA.R
@@ -3,7 +3,7 @@
 
 library(tidyverse)
 # read in data
-pca <- read_table2("data/sorghum.eigenvec", col_names = FALSE)
+pca <- read_table("data/00.pca/filtered_snps_pca.eigenval", col_names = FALSE)
 eigenval <- scan("data/sorghum.eigenval")
 
 # sort out the pca data
@@ -14,4 +14,3 @@ names(pca)[1] <- "id"
 names(pca)[2:ncol(pca)] <- paste0("PC", 1:(ncol(pca)-1))
 library(ggplot2)
 ggplot(data=pca, aes(PC1,PC2)) + geom_point()
-
