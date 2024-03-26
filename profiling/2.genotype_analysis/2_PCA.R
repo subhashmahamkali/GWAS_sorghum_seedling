672c70e460aef4610473d379690277e1a0a433ef
subhashmahamkali <mahankalisubhash03@gmail.com>
2024-03-26 13:48:42 -0500

    basic statistics

--- profiling/2.genotype_analysis/2_PCA.R
+++ profiling/2.genotype_analysis/2_PCA.R
@@ -1,11 +1,11 @@
 #https://speciationgenomics.github.io/pca/
 #pca was conducted using VCF file susing plink on HCC.
-
-library(tidyverse)
+#library(tidyverse)
+library(data.table)
 # read in data
-pca <- read_table("data/00.pca/filtered_snps_pca.eigenval", col_names = FALSE)
-eigenval <- scan("data/sorghum.eigenval")
+pca <- read_table("data/00.pca/filtered_snps_pca.eigenvec", col_names = TRUE)
 
+#eigenval <- scan("data/sorghum.eigenval")
 # sort out the pca data
 # remove nuisance column
 pca <- pca[,-1]
